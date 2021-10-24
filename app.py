75a2cea8800d19340ae4deb728dc3cf1f6c975b7
rairom <rairom@sonity.net>
2021-10-24 20:44:25 +0200

    query works

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, render_template
 import Bot
+import wikipedia as wk
+import wolframalpha
+
+APIkey = "R62U64-JYYVGAVHHU"
+wolf = wolframalpha.Client(APIkey)
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -22,13 +29,19 @@ def chat():
                     response = Bot.greeting(user_response)
                     return response
                 else:
+                    try:
+                        res = wolf.query(user_response)
+                        response = next(res.results).text
+                    except:
+                        try:
+                          response = wk.summary(user_response, sentences = 3)
+                        except:
+                          response = "May you try to refine your query, I am still learning please be patient with me"
 
-                    response = Bot.response(user_response)
-                    Bot.sent_tokens.remove(user_response)
                     return response
         else:
             flag = False
-            response = "Bye! take care, and if you want to say anything more you can leave a voice note"
+            response = "Good Bye! "
             return response
 
 if __name__ == "__main__":
